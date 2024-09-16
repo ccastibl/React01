86ad7746a8bdb2b8504e1c87486b247c488a4ffe
ccastibl <ccastibl@cuc.edu.co>
2024-09-16 09:41:20 -0500

    Proyecto react 01

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import miLogo from "./assets/img/logoReact.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="logo de react" />
+        <h1>¡¡¡Hola mundo!!!</h1>
+        <p>Este es mi primer proyecto en react</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
